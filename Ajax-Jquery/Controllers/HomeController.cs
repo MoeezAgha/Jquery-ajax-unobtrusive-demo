510afb0253ab17756bbea9f2bae348dda7af5665
Moeez <aghamoeez@gmail.com>
2022-08-10 12:57:03 -0400

    Add project files.

---
+++ Ajax-Jquery/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Ajax_Jquery.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace Ajax_Jquery.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult User()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult User(UserInfo userInfo)
+        {
+
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+
+    public class UserInfo
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+
+        public DateOnly DOB { get; set; }
+        public IFormFile? doc { get; set; }
+
+    }
+}
